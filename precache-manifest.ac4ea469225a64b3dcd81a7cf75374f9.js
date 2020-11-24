d298a8ed912bfe112ac5aedf64dda18600e37998
Nomes27 <nomi_smith18@hotmail.com>
2020-11-24 11:38:42 +0000

    Updates

---
+++ precache-manifest.ac4ea469225a64b3dcd81a7cf75374f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8f6c1ffd492c52941df0a0bac4a276",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "a4e9448f8131cdd088b6",
+    "url": "/to-do-list/static/css/main.058518ff.chunk.css"
+  },
+  {
+    "revision": "d571e64ad2712b1e3bbc",
+    "url": "/to-do-list/static/js/2.544db46f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.544db46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4e9448f8131cdd088b6",
+    "url": "/to-do-list/static/js/main.e2990e25.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  },
+  {
+    "revision": "e4f4016c4cb5b0d5fedee3dda7280af6",
+    "url": "/to-do-list/static/media/cacti-pale.e4f4016c.jpeg"
+  }
+]);
